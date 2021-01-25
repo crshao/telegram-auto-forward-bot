73f1edc4f2101c94f085dba77d56e2bdfbdc5809
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-01-25 22:42:21 +0700

    deploying to heroku

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1561496403:AAHIq_VyAQP24EzR_xagg85RoY6nzgYRn54"
 bot_username = "@hao_autoforwardbot"
-URL = "the heroku app link that we will create later"
+URL = "https://telegram-auto-forward-bot.herokuapp.com/"
